e91c846a1f93559ac1226fcca0cc0a8096239b35
ramkrishna05 <108403796+ramkrishna05@users.noreply.github.com>
2023-07-26 09:51:43 +0530

    first update

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class RWOnly {
-    
+    private String name;
+
+    public void setName(String name)
+    {
+        this.name = name;
+
+    }
+    public String getName()
+    {
+        return name;
+    }
+
 }
